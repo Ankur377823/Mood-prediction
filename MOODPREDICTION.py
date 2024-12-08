3a20e380f4248a0160ee0d9a38a1fb7ffab46be3
Ankur377823 <ankurtp123@gmail.com>
2024-12-08 22:27:49 +0530

    Update MOODPREDICTION.py

--- MOODPREDICTION.py
+++ MOODPREDICTION.py
@@ -10,7 +10,7 @@ mood_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: '
 model = load_model('image.keras')  # Ensure path is correct
 
 # Streamlit app layout
-st.title('CNN Model Deployment')
+st.title('MOOD PREDICTION ')
 st.write('Upload an image for prediction')
 
 # File uploader for image
